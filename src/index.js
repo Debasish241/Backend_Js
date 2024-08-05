89b8571fb5a9b5209b2119ecc7b2cb30baff982b
Debasish Debnath <ddebnath241@gmail.com>
2024-08-06 00:14:01 +0530

    setup done

--- src/index.js
+++ src/index.js
@@ -1,58 +1,14 @@
-// require('dotenv').config()
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+// Connect to MongoDB
 
-// const express = require('express')
 import express from "express"
-const app = express()
-// const port = 3000
-
-const port = process.env.PORT || 3000;
-
-app.get('/',(req,res)=>{
-res.send("Hello world");
-})
-
-
-//get a list of 5 jokes
-// app.get('/api/jokes',(req,res)=>{
-//     const jokes=[
-//         {
-//             id: 1,
-//             title: 'What do you call a country where the clock ticks on 12 noon?',
-//             content: 'The 12-hour Clock'
-//         },
-//         {
-//             id: 2,
-//             title: 'Why did the chicken cross the road?',
-//             content: 'To get to the other side!'
-//         },
-//         {
-//             id: 3,
-//             title: 'What do you call a farmer who sells his tomatoes?',
-//             content: 'The fruit seller'
-//         },
-//         {
-//             id: 4,
-//             title: 'What did the ticklish baby say when it got a new pillow?',
-//             content: 'What do you call it, Boo-Boo?'
-//         }
-//     ]
-//     res.send(jokes)
-// })
-
-
-// app.get('/twitter',(req,res)=>{
-//     res.send("Twitter API Endpoint");
-// })
-
-// app.get('/login',(req,res)=>{
-//     res.send('<h1>please login</h1>')
-// })
-
-
-// app.get('/youtube',(req,res)=>{
-// res.send("<h2>Hello word</h2>")
-// })
-
-app.listen(process.env.PORT,()=>{
-    console.log(`Example app listening on port ${port}`)
-})
+const app=express()
+(async()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)        
+    } catch (error) {
+        console.error("ERROR: ",error)
+        throw error
+    }
+})()
