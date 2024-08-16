10befc00433f04052f94ef3b3dd209cc69932c86
DebasishDebnath <debasish.debnath@iemlabs.com>
2024-08-16 13:44:39 +0530

    add subscription model

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,10 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import {
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  registerUser,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -7,18 +12,16 @@ const router = Router();
 
 router.route("/register").post(
   upload.fields([
-      { name: "coverImage", maxCount: 1 },
-      { name: "avatar", maxcount: 1 }
+    { name: "coverImage", maxCount: 1 },
+    { name: "avatar", maxcount: 1 },
   ]),
   registerUser
 );
 
-
-router.route("/login").post(loginUser)
+router.route("/login").post(loginUser);
 
 //secured routes
-router.route("/logout").post(verifyJWT, logoutUser)
-router.route("refresh-token").post(refreshAccessToken)
-
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("refresh-token").post(refreshAccessToken);
 
 export default router;
